d66616fb645d1b1ad0813d9fa134af2f1fb7df0d
Brendan C <115911754+BChharaw@users.noreply.github.com>
2025-09-14 13:00:20 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import PrismProject from "./components/PrismProject";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Portfolio />} />
           <Route path="/prism" element={<PrismProject />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
